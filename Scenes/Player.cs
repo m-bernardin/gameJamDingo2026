91af21fa8b71fcb0f98dda847d7af8164d5f5f4d
maddysiwick <maddy.siwick@icloud.com>
2026-01-10 12:20:40 -0500

    basic versions of player and planet

---
+++ Scenes/Player.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+using System.Collections;
+
+public partial class Player : Node2D
+{
+	int[] Stats;
+	int[] items;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public int GetOdds(int[] StatsNeeded,int ChallengeType)
+	{
+		ArrayList PlayerStats=new ArrayList();
+		foreach(int i in StatsNeeded)
+		{
+			PlayerStats.Add(Stats[i]);
+		}
+		//formula
+	}
+}
