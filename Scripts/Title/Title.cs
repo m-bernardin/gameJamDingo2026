87b66c4961559f7da4691bb6c6819580c886d4e6
m-bernardin <mjrb4mjrb@gmail.com>
2026-01-10 13:34:17 -0500

    optimized Title scripts

--- Scripts/Title/Title.cs
+++ Scripts/Title/Title.cs
@@ -4,23 +4,15 @@ using System.Collections.Generic;
 
 public partial class Title : Area2D
 {
-	Play play;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		play=new Play();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		CheckMouseInput();
 	}
-
-	private void CheckMouseInput()
-	{
-	}
-
 	private void PlayPressed()
 	{
 		GD.Print("play pressed");
