fcc16329942d2331a593522e424759ee468ab8cf
m-bernardin <mjrb4mjrb@gmail.com>
2026-01-10 22:51:31 -0500

    pieces can collide?

--- Scripts/MiniGame/MiniGameManager.cs
+++ Scripts/MiniGame/MiniGameManager.cs
@@ -7,6 +7,12 @@ public partial class MiniGameManager : Node2D
 	private Player player;
 	public override void _Ready()
 	{
+		var scene1=ResourceLoader.Load<PackedScene>("res://Scenes/MiniGame/piece1.tscn");
+		var piece1=scene1.Instantiate<GamePiece>();
+		piece1.Position=new Vector2(550,400);
+		var scene2=ResourceLoader.Load<PackedScene>("res://Scenes/MiniGame/piece1.tscn");
+		var piece2=scene2.Instantiate<GamePiece>();
+		piece2.Position=new Vector2(700,800);
 	}
 	public override void _Process(double delta)
 	{
