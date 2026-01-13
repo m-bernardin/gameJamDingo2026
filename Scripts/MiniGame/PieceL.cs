ebbd3759ed0658bcfa99f95628ef4ed452ccefed
m-bernardin <mjrb4mjrb@gmail.com>
2026-01-12 20:15:38 -0500

    game functionally done???? yayyy

--- Scripts/MiniGame/PieceL.cs
+++ Scripts/MiniGame/PieceL.cs
@@ -1,13 +1,13 @@
 using Godot;
 using System;
 
-public partial class Piece2 : GamePiece
+public partial class PieceL : GamePiece
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		base._Ready();
-		size=4;
+		size=6;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
